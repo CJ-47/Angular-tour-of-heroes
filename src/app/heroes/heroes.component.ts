053eb0eda18b7e3c2e64518c6c92d6879c2826e2
CJ-47 <kandari.hritik89@gmail.com>
2023-06-11 12:16:43 +0530

    NGModule double binding works

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,8 +3,10 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
-})
+  styleUrls: ['./heroes.component.css'],
+
+}
+)
 export class HeroesComponent {
   hero: Hero = {
     id: 1,
