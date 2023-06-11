045708e776a75f7cbd913c2b00273ca654a594b2
CJ-47 <kandari.hritik89@gmail.com>
2023-06-11 15:18:50 +0530

    Router for Navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+const routes: Routes = [ { path: 'detail/:id', component: HeroDetailComponent }, { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+{ path: 'heroes', component: HeroesComponent } , {
+path: 'dashboard' , component: DashboardComponent}  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 0o1, name: 'Dr. Nice' },
+  { id: 0o2, name: 'Bombasto' },
+  { id: 0o3, name: 'Celeritas' },
+  { id: 0o4, name: 'Magneta' },
+  { id: 0o5, name: 'RubberMan' },
+  { id: 0o6, name: 'Dynama' },
+  { id: 0o7, name: 'Dr. IQ' },
+  { id: 0o10, name: 'Magma' },
+  { id: 0o11, name: 'Tornado' }
 ];
